925a2ed57363e378898bde45fbd57dcf8dbb16ee
MatheusAndrade60 <matheus.asilva60@gmail.com>
2024-06-24 21:40:05 -0300

    add-XUnit

---
+++ ProjectNet.Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+namespace ProjectNet.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+            //Arrenge
+
+            //Act
+
+            //Assert
+        }
+
+        [Fact]
+        public void Test2()
+        {
+
+            //Arrenge
+
+            //Act
+
+            //Assert
+        }
+    }
+}

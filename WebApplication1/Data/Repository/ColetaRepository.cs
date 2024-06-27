4d03225e8a7867eb82f8873244cb990ab6fab0c5
arthurwrx <arthurwrx@outlook.com>
2024-06-27 00:25:44 -0300

    feat: Criado Repository

---
+++ WebApplication1/Data/Repository/ColetaRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Data.Repository
+{
+    public class RepresetanteRepository : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
